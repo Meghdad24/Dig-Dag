2ca24073720ee587dc0f7fc1dc76bdb739816f2c
pourya ramezani <pourya.ramezani.21224@gmail.com>
2022-06-13 11:01:34 +0430

    Full project load

---
+++ src/main/java/com/personal/graphic/game/digdag/HelloApplication.java
@@ -0,0 +1,23 @@
+package com.personal.graphic.game.digdag;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(new Pane(), 640, 480);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/personal/graphic/game/digdag/HelloController.java
@@ -0,0 +1,14 @@
+package com.personal.graphic.game.digdag;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
